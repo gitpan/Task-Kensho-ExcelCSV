2388f301dec4cab096974e103dc5bc3102a6c192
Karen Etheridge <ether@cpan.org>
2014-01-21 06:18:03 +0000

    Import of ETHER/Task-Kensho-ExcelCSV-0.34002-TRIAL from CPAN.
    gitpan-cpan-distribution: Task-Kensho-ExcelCSV
    gitpan-cpan-version:      0.34002-TRIAL
    gitpan-cpan-path:         ETHER/Task-Kensho-ExcelCSV-0.34002-TRIAL.tar.gz
    gitpan-cpan-author:       ETHER
    gitpan-cpan-maturity:     developer

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Task::Kensho::ExcelCSV - Excel/CSV
 
 # VERSION
 
-version 0.34001
+version 0.34002
 
 # SYNOPSIS
 
--- lib/Task/Kensho/ExcelCSV.pm
+++ lib/Task/Kensho/ExcelCSV.pm
@@ -4,8 +4,8 @@ package Task::Kensho::ExcelCSV;
 BEGIN {
   $Task::Kensho::ExcelCSV::AUTHORITY = 'cpan:PERIGRIN';
 }
-# git description: v0.34-TRIAL-37-g47458f2
-$Task::Kensho::ExcelCSV::VERSION = '0.34001'; # TRIAL
+# git description: v0.34001-TRIAL-11-g86ed10b
+$Task::Kensho::ExcelCSV::VERSION = '0.34002'; # TRIAL
 # ABSTRACT: Excel/CSV
 
 __END__
@@ -20,7 +20,7 @@ Task::Kensho::ExcelCSV - Excel/CSV
 
 =head1 VERSION
 
-version 0.34001
+version 0.34002
 
 =head1 SYNOPSIS
 
